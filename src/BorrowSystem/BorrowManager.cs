eaea423c6123bf6f55b2f7132f30279ddfd50d1c
José Luis Terán Rocha <jose.luis.teran.rocha.2@gmail.com>
2024-08-22 18:36:22 -0400

    refactor(borrow): add exception handling for borrow operations

--- src/BorrowSystem/BorrowManager.cs
+++ src/BorrowSystem/BorrowManager.cs
@@ -1,53 +1,56 @@
 using BookSystem;
+using Core.Exceptions;
 using userSystem.Concrete;
 
 namespace BorrowSystem;
-public class BorrowManager{
+
+public class BorrowManager
+{
     List<Borrow> Borrows;
+
     public BorrowManager(List<Borrow> borrows){
-        this.Borrows = borrows;
+        Borrows = borrows;
     }
 
     public void AddBorrow(Patron patron, Book book, DateTime borrowDate, DateTime dueDate)
     {
-        Borrow borrow = new Borrow(patron, book, borrowDate, dueDate);
-        if(book.Stock > 0){
-            Borrows.Add(borrow);
-            Console.WriteLine($"Successfull Borrow of {book.Title} by {patron.getName()}"); 
-        }else{
-            Console.WriteLine($"{book.Title} Has no available copies the stock is {book.Title}");
+        if (borrowDate > dueDate)
+        {
+            throw new InvalidDateRangeException("Borrow date cannot be after due date.");
         }
-           
+        
+        Borrow borrow = new Borrow(patron, book, borrowDate, dueDate);
+        Borrows.Add(borrow);
     }
 
     public void RemoveBorrow(string patronName, string bookTitle)
     {
-       Borrow? borrow = FindBorrow(patronName, bookTitle);
-        if (borrow != null)
-        {
-            Borrows.Remove(borrow);
-        } 
+        Borrow? borrow = FindBorrow(patronName, bookTitle);
+        Borrows.Remove(borrow);
     }
 
     public Borrow? FindBorrow(string patronName, string bookTitle )
     {
         foreach (Borrow borrow in Borrows)
+        {
+            string currentPatron = borrow.GetPatron().getName();
+            string currentBook = borrow.GetBook().Title;
+
+            if (currentPatron == patronName && currentBook == bookTitle)
             {
-                string currentPatron = borrow.GetPatron().getName();
-                string currentBook = borrow.GetBook().Title; 
-                if (currentPatron == patronName && currentBook == bookTitle)
-                {
-                    return borrow;
-                }
+                return borrow;
             }
+        }
+
         return null;        
     }
 
     public void ActiveBorrowsFromPatron(Patron patron){
-        Console.WriteLine("lIST OF NOT RETURNED BORROWS");
+        Console.WriteLine("LIST OF NOT RETURNED BORROWS");
+
         foreach (Borrow borrow in Borrows)
         {
-            if(borrow.GetPatron().getName().Equals(patron.getName()) && borrow.GetDelivered() == false)
+            if (borrow.GetPatron().getName().Equals(patron.getName()) && borrow.GetDelivered() == false)
             {
                 borrow.BorrowDetails();
             }
