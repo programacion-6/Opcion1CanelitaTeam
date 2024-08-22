a376b78516cc8a6c5616f0220f0ca4d12dd0195c
José Luis Terán Rocha <jose.luis.teran.rocha.2@gmail.com>
2024-08-22 17:14:42 -0400

    feat: implement book not found class exception

---
+++ src/Core/Exceptions/BookNotFoundException.cs
@@ -0,0 +1,11 @@
+using Core.Handlers;
+
+namespace Core.Exceptions;
+
+public class BookNotFoundException : BaseException
+{
+    public BookNotFoundException(string message) 
+    : base(message, Severity.MEDIUM)
+    {
+    }
+}
