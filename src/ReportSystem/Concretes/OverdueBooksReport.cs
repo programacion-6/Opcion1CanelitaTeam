fb65b67a32f7cd79ac4fb251588d9d007228b863
José Luis Terán Rocha <jose.luis.teran.rocha.2@gmail.com>
2024-08-23 17:43:45 -0400

    feat(reports): add handlers and loggers for report operations

--- src/ReportSystem/Concretes/OverdueBooksReport.cs
+++ src/ReportSystem/Concretes/OverdueBooksReport.cs
@@ -1,13 +1,18 @@
 using BorrowSystem;
+using Core.Handlers;
 using Opcion1CanelitaTeam.ReportSystem.Abstracts;
 
 namespace Opcion1CanelitaTeam.ReportSystem.Concretes;
 
 public class OverdueBooksReport : BaseReport
 {
+    private readonly ILogService _logService;
+
     public OverdueBooksReport(BorrowManager borrowManager) 
         : base(borrowManager)
-    { }
+    { 
+        _logService = new LogService();
+    }
 
     public override void GenerateReport()
     {
@@ -16,13 +21,32 @@ public class OverdueBooksReport : BaseReport
 
     public void OverdueBooksListReport()
     {
-        GenerateReport();
-        foreach (Borrow borrow in _borrowManager.GetBorrows())
+        try
         {
-            if (DateTime.Now > borrow.GetDueDate())
+            GenerateReport();
+
+            List<Borrow> borrowList = _borrowManager.GetBorrows();
+            if (borrowList == null)
             {
-                borrow.BorrowDetails();
+                throw new InvalidOperationException("Failed to retrieve borrow records. The list is null.");
             }
-        }        
+
+            foreach (Borrow borrow in borrowList)
+            {
+                if (DateTime.Now > borrow.GetDueDate())
+                {
+                    borrow.BorrowDetails();
+                }
+            }
+        }
+        catch (InvalidOperationException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An unexpected error occurred while generating the borrowed books report. Please contact your administrator.");
+            _logService.LogError(Severity.HIGH, $"{ex.Message}");
+        }
     }
 }
